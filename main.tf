454cb9dd6dbbdf384b74ed0c6ca27a4c77a307e5
Uladzimir Sakhonchyk <33266845+zm99by@users.noreply.github.com>
2024-08-22 23:47:47 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -36,3 +36,109 @@ terraform {
     prefix = "jenkins-gke"
   }
 }
+
+provider "kubernetes" {
+  host                   = google_container_cluster.primary.endpoint
+  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
+  token                  = data.google_client_config.default.access_token
+}
+
+resource "kubernetes_namespace" "jenkins" {
+  metadata {
+    name = "jenkins"
+  }
+}
+
+resource "kubernetes_deployment" "jenkins" {
+  metadata {
+    name      = "jenkins"
+    namespace = kubernetes_namespace.jenkins.metadata[0].name
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "jenkins"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "jenkins"
+        }
+      }
+
+      spec {
+        container {
+          name  = "jenkins"
+          image = "jenkins/jenkins:lts"
+
+          ports {
+            container_port = 8080
+          }
+
+          resources {
+            limits {
+              memory = "1024Mi"
+              cpu    = "500m"
+            }
+          }
+
+          volume_mount {
+            name       = "jenkins-home"
+            mount_path = "/var/jenkins_home"
+          }
+        }
+
+        volume {
+          name = "jenkins-home"
+
+          persistent_volume_claim {
+            claim_name = kubernetes_persistent_volume_claim.jenkins_pvc.metadata[0].name
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "jenkins" {
+  metadata {
+    name      = "jenkins"
+    namespace = kubernetes_namespace.jenkins.metadata[0].name
+  }
+
+  spec {
+    selector = {
+      app = "jenkins"
+    }
+
+    type = "LoadBalancer"
+
+    port {
+      port        = 8080
+      target_port = 8080
+    }
+  }
+}
+
+resource "kubernetes_persistent_volume_claim" "jenkins_pvc" {
+  metadata {
+    name      = "jenkins-pvc"
+    namespace = kubernetes_namespace.jenkins.metadata[0].name
+  }
+
+  spec {
+    access_modes = ["ReadWriteOnce"]
+
+    resources {
+      requests = {
+        storage = "10Gi"
+      }
+    }
+  }
+}
+
