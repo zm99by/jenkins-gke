82f87a8b1ed866bf874de3a63dcc426f2d4fa000
Uladzimir Sakhonchyk <33266845+zm99by@users.noreply.github.com>
2024-08-22 23:45:02 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,31 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_container_cluster" "primary" {
+  name     = var.cluster_name
+  location = var.region
+
+  node_config {
+    machine_type = var.machine_type
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+  }
+
+  initial_node_count = var.node_count
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+  cluster    = google_container_cluster.primary.name
+  location   = google_container_cluster.primary.location
+  node_count = var.node_count
+
+  node_config {
+    machine_type = var.machine_type
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+  }
+}
